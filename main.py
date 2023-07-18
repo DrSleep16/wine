a4a3ae6c0d04286ae4807c796a25fdf6ffc641f9
Dmitry <Dr.Sleep1995@gmail.com>
2023-07-18 13:53:56 +0300

    Добавил винный магазин

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
+import pandas
+from collections import defaultdict
 
 
+def years_have_passed(starting_year):
+    age = datetime.datetime.now().year - starting_year
+    if 11 <= age <= 14:
+        year_ending = 'лет'
+    elif age % 10 == 1:
+        year_ending = 'год'
+    elif 2 <= age % 10 <= 4:
+        year_ending = 'года'
+    elif 5 <= age % 10 <= 9 or age % 10 == 0:
+        year_ending = 'лет'
+    else:
+        year_ending = 'Некорректная дата'
+    return str(age) + ' ' + year_ending
+
+
+excel_data = pandas.read_excel('wine3.xlsx')
+excel_data.fillna('', inplace=True)
+wine_dict = defaultdict(list)
+for row in excel_data.to_dict(orient='records'):
+    category = row['Категория']
+    del row['Категория']
+    wine_dict[category].append(row)
+wine_dict = dict(wine_dict)
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html'])
+)
+template = env.get_template('template.html')
+rendered_page = template.render(
+    age=years_have_passed(1920),
+    wines=wine_dict
+)
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
