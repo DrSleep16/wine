99362589c62c0cf92d5a625dce3e529a416be995
Dmitry <Dr.Sleep1995@gmail.com>
2023-07-21 12:28:40 +0300

    Все вызовы load_dotenv собраны внутри ifmain

--- main.py
+++ main.py
@@ -20,25 +20,25 @@ def calculate_years_passed(starting_year):
     return f"{age} {year_ending}"
 
 
-excel_data = pandas.read_excel('wine3.xlsx')
-excel_data.fillna('', inplace=True)
-wine_dict = defaultdict(list)
-for row in excel_data.to_dict(orient='records'):
-    category = row['Категория']
-    del row['Категория']
-    wine_dict[category].append(row)
-wine_dict = dict(wine_dict)
-
-env = Environment(
-    loader=FileSystemLoader('.'),
-    autoescape=select_autoescape(['html'])
-)
-template = env.get_template('template.html')
-rendered_page = template.render(
-    age=calculate_years_passed(1920),
-    wines=wine_dict
-)
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+if __name__ == '__main__':
+    excel_data = pandas.read_excel('wine3.xlsx')
+    excel_data.fillna('', inplace=True)
+    wine_dict = defaultdict(list)
+    for row in excel_data.to_dict(orient='records'):
+        category = row['Категория']
+        del row['Категория']
+        wine_dict[category].append(row)
+    wine_dict = dict(wine_dict)
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html'])
+    )
+    template = env.get_template('template.html')
+    rendered_page = template.render(
+        age=calculate_years_passed(1920),
+        wines=wine_dict
+    )
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
